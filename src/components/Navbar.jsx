2215c0967c849eeae1a99ac77c64c8d276ef5a63
Andrew Dang <andrew.dang17@gmail.com>
2024-05-19 16:00:31 -0700

    add container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,75 +25,83 @@ const Navbar = () => {
       role="navigation"
       aria-label="main navigation"
     >
-      <div className="navbar-brand">
-        <a
-          href="https://instagram.com/brentons_barkyard"
-          target="_blank"
-          className="is-flex is-align-items-center ml-3 mr-3 "
-        >
-          <InstagramIcon />
-        </a>
-        <a
-          role="button"
-          onClick={toggleMenu}
-          className={`navbar-burger ${menuClass}`}
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div className={`navbar-menu ${menuClass}`}>
-        <div className="navbar-end">
-          <BookingModal isNavItem={true} />
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link" onClick={() => scrollTo("services")}>
-              Services
-            </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item" onClick={() => scrollTo("boarding")}>
-                Boarding
-              </a>
-              <a className="navbar-item" onClick={() => scrollTo("daycare")}>
-                Daycare
-              </a>
-              <a className="navbar-item" onClick={() => scrollTo("park-visit")}>
-                Park Visit
-              </a>
-              <a
-                className="navbar-item"
-                onClick={() => scrollTo("drop-in-visit")}
-              >
-                Drop-In Visit
-              </a>
-              <a
-                className="navbar-item"
-                onClick={() => scrollTo("pick-up-drop-off")}
-              >
-                Pickup/Dropoff
-              </a>
-              <a className="navbar-item" onClick={() => scrollTo("extra-care")}>
-                Extra Care
-              </a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item" onClick={() => scrollTo("policy")}>
-                Policy
-              </a>
-              <a className="navbar-item" onClick={() => scrollTo("policy")}>
-                Terms & Conditions
-              </a>
-            </div>
-          </div>
-
-          <a className="navbar-item" onClick={() => scrollTo("about")}>
-            About
+      <div className="container">
+        <div className="navbar-brand">
+          <a
+            href="https://instagram.com/brentons_barkyard"
+            target="_blank"
+            className="is-flex is-align-items-center ml-3 mr-3 "
+          >
+            <InstagramIcon />
           </a>
+          <a
+            role="button"
+            onClick={toggleMenu}
+            className={`navbar-burger ${menuClass}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div className={`navbar-menu ${menuClass}`}>
+          <div className="navbar-end">
+            <BookingModal isNavItem={true} />
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link" onClick={() => scrollTo("services")}>
+                Services
+              </a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item" onClick={() => scrollTo("boarding")}>
+                  Boarding
+                </a>
+                <a className="navbar-item" onClick={() => scrollTo("daycare")}>
+                  Daycare
+                </a>
+                <a
+                  className="navbar-item"
+                  onClick={() => scrollTo("park-visit")}
+                >
+                  Park Visit
+                </a>
+                <a
+                  className="navbar-item"
+                  onClick={() => scrollTo("drop-in-visit")}
+                >
+                  Drop-In Visit
+                </a>
+                <a
+                  className="navbar-item"
+                  onClick={() => scrollTo("pick-up-drop-off")}
+                >
+                  Pickup/Dropoff
+                </a>
+                <a
+                  className="navbar-item"
+                  onClick={() => scrollTo("extra-care")}
+                >
+                  Extra Care
+                </a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item" onClick={() => scrollTo("policy")}>
+                  Policy
+                </a>
+                <a className="navbar-item" onClick={() => scrollTo("policy")}>
+                  Terms & Conditions
+                </a>
+              </div>
+            </div>
+
+            <a className="navbar-item" onClick={() => scrollTo("about")}>
+              About
+            </a>
+          </div>
         </div>
       </div>
     </nav>
